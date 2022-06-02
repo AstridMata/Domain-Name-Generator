35d69bea4f5da1d33bc80fb51a880f8959659b8e
IFRinnegan <chifvs@gmail.com>
2022-06-02 01:44:06 +0000

    Add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+
+for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+        for (let z = 0; z < noun.length; z++) {
+            console.log(pronoun[y]+adj[x]+noun[z]+".com")
+        }
+    }
+}
